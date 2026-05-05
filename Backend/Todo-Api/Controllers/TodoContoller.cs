8777d8a53ba624ad9bf0cdc87250c6232a3a262f
Melker-Karlsson <melker.karlsson041008@gmail.com>
2026-05-05 15:49:44 +0200

    added controller code

--- Backend/Todo-Api/Controllers/TodoContoller.cs
+++ Backend/Todo-Api/Controllers/TodoContoller.cs
@@ -1,19 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 
+[Route("api/todo")]
+[ApiController]
 public class TodoController : ControllerBase
 {
+    ITodoService _todoService;
+
+    public TodoController(ITodoService todoService)
+    {
+        _todoService = todoService;
+    }
+
+    [HttpGet]
     public async Task<ActionResult> GetTodoList()
     {
-        return Ok();
+        return Ok(await _todoService.GetTodoListAsync());
     }
 
-    public async Task<ActionResult> SetTodoObject(DTOTodo todo)
+    [HttpPost]
+    public async Task<ActionResult> PostTodoObject([FromBody] DTOTodo todo)
     {
-        return Ok();
+        try
+        {
+            return Ok(await _todoService.PostTodoAsync(todo));
+        }
+        catch(Exception eror)
+        {
+            return BadRequest(eror.Message);
+        }
     }
 
-    public async Task<ActionResult> EditTodoObject(int id, DTOTodo todo)
+    [HttpPatch]
+    public async Task<ActionResult> PatchTodoObject([FromBody] int id, [FromBody] DTOTodo todo)
     {
-        return Ok();
+        try
+        {
+            return Ok(await _todoService.PatchTodoAsync(id, todo));
+        }
+        catch(Exception eror)
+        {
+            return BadRequest(eror.Message);
+        }
     }
 }
