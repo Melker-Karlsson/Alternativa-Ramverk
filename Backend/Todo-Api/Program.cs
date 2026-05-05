52a604fbeb1f6aa761dc1cc545c77a17f6b33121
Melker-Karlsson <melker.karlsson041008@gmail.com>
2026-05-05 22:07:13 +0200

    Added CORS

--- Backend/Todo-Api/Program.cs
+++ Backend/Todo-Api/Program.cs
@@ -18,6 +18,16 @@ builder.Services.AddDbContext<AppDBContext>(options =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 
