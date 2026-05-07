23751ac43a006e476f183c4f669ab93bb6ef12d3
Melker-Karlsson <melker.karlsson041008@gmail.com>
2026-05-07 11:53:08 +0200

    Added api-methods to app

--- Backend/Todo-Api/obj/Debug/net10.0/Todo-Api.AssemblyInfo.cs
+++ Backend/Todo-Api/obj/Debug/net10.0/Todo-Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068e4e8b737abd75bfb5cfc78b623f091255c748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0fd5edcdbbe2bf70bec2d6d429291371c88cde4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontend/todovue/src/App.vue
+++ Frontend/todovue/src/App.vue
@@ -1,15 +1,61 @@
 <template>
-
+  <div id="app">
+    <!-- Todo app content will go here -->
+  </div>
 </template>
 
 <script>
-
+import GetTodo from './Global/GetTodo';
+import PostTodo from './Global/PostTodo';
+import PatchTodo from './Global/PatchTodo';
+import DeleteTodo from './Global/DeleteTodo';
 
 export default {
   name: 'App',
+
   components: {
 
-  }
+  },
+
+  data(){
+    return{
+      todos: []
+    }
+  },
+
+  methods: {
+    async loadTodos(){
+      this.todos = await GetTodo();
+      console.log(this.todos);
+    },
+
+    delete(id){
+      DeleteTodo(id);
+      this.todos.update(todos => 
+          todos.filter(todo => todo.id !== id)
+      )
+    },
+
+    update(todo){
+      PatchTodo(todo.id, {
+        title: todo.title,
+        context: todo.context
+      })
+      this.todos.update(todos => 
+        todos.map(t => t.id === todo.id? todo : t)
+      )
+    },
+
+    async add(todo){
+      const newTodo = await PostTodo(todo);
+      if(newTodo === null) return;
+      this.todos.update(todos => [...todos, newTodo])
+    }
+  },
+
+  created(){
+    this.loadTodos();
+  },
 }
 </script>
 
